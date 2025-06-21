c70feb8c7bb646cae4d3abc713ae07dd60e39e02
GregEnriquez <gregjames.enriquez08@gmail.com>
2025-06-21 19:23:20 +0800

    Add project files.

---
+++ FinalProject/Controllers/InventoryController.cs
@@ -0,0 +1,19 @@
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Inventory> items = new List<Inventory>();
+            return View(items);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinalProject/Models/Inventory.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalProject.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int AmountSold { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime DateSold { get; set; }
+        public string ItemType { get; set; }
+    }
+}
